7397469cca4c839f4f58a14f85b9c61ae1245561
XuanYu1725 <2678858735@qq.com>
2025-09-05 20:02:54 +0800

    添加初始的 Java 程序示例，打印 "Hello World!" 到控制台

---
+++ Common/Example01_debug_and_compile/src/Targat.java
@@ -0,0 +1,10 @@
+// using javac and java commands to compile and run a simple Java program
+// to compile: javac Target.java
+// to run: java Target
+// This program prints "Hello World!" to the console
+
+public class Target{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
