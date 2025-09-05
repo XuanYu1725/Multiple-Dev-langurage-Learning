85567d54b92318f40ce1ef3007e3031d1181f75d
XuanYu1725 <2678858735@qq.com>
2025-09-05 20:24:32 +0800

    Refactor code structure for improved readability and maintainability

--- Common/Example01_debug_and_compile/src/Target.cpp
+++ Common/Example01_debug_and_compile/src/Target.cpp
@@ -1,6 +1,6 @@
 // using g++ and ./Result.exe commands to compile and run a simple C++ program
-// to compile: g++ Target.cpp -o Result
-// to run: ./Result.exe
+// to compile: g++ Target.cpp -o Target
+// to run: ./Target.exe
 // This program prints "Hello World!" to the console
 
 #include<iostream>
