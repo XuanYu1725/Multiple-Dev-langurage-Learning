1ac2d2d562d9e9c5ebe6832c076dbddfc03c0ba0
XuanYu1725 <2678858735@qq.com>
2025-09-06 23:31:31 +0800

    修改Example03第三个需求的Java实现，与C/C++的基本逻辑一致，添加了对Java的讲解

--- Common/Example03_console_input/java/src/Input03.java
+++ Common/Example03_console_input/java/src/Input03.java
@@ -4,17 +4,11 @@ import java.util.Scanner;
 public class Input03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter numbers (space-separated):  ");
+        System.out.print("Enter numbers (space-separated) Type Ctrl + D to end:  ");
         int sum = 0;
-        String line = scanner.nextLine();
-        String[] numbers = line.split(" ");
-        for (String numStr : numbers) {
-            try {
-                int num = Integer.parseInt(numStr);
-                sum += num;
-            } catch (NumberFormatException e) {
-                // Ignore invalid numbers
-            }
+        while (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            sum += num;
         }
         System.out.println("Sum: " + sum);
         scanner.close();
