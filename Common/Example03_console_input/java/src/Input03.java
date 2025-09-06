77128303004a0990d8489be70d9fb726dfad6038
XuanYu1725 <2678858735@qq.com>
2025-09-07 07:17:34 +0800

    经高人指点，现在可以将Example03 Java的逻辑和C/C++统一了

--- Common/Example03_console_input/java/src/Input03.java
+++ Common/Example03_console_input/java/src/Input03.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class Input03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter numbers (space-separated) Type Ctrl + D to end:  ");
+        System.out.print("Enter numbers (space-separated): ");
+        String line = scanner.nextLine();
+        Scanner lineScanner = new Scanner(line);
         int sum = 0;
-        while (scanner.hasNextInt()) {
-            int num = scanner.nextInt();
+        while (lineScanner.hasNextInt()) {
+            int num = lineScanner.nextInt();
             sum += num;
         }
         System.out.println("Sum: " + sum);
         scanner.close();
+        lineScanner.close();
     }
 }
